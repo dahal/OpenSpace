27e8d42071c21e5120c04f7cb8a29fc2d79dc13a
Puru Dahal <pdahal@me.com>
2013-12-13 00:33:37 -0600

    Stripe payment dome

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -27,39 +27,28 @@ class CartsController < ApplicationController
   end
 
 
-
-  
-
-  # GET /carts/new
-  # GET /carts/new.json
-  def new
-    @cart = Cart.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @cart }
-    end
-  end
-
-  # GET /carts/1/edit
-  def edit
-    @cart = Cart.find(params[:id])
-  end
-
-  # POST /carts
-  # POST /carts.json
+#New & Create methods are from stripe
   def create
-    @cart = Cart.new(params[:cart])
+    # Amount in cents
+    @amount = params[:amount_to_capture]#check _cart.html.erb for hidden form [:amount_to_capture]
+    @user = User.find(session[:user])
 
-    respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
-        format.json { render json: @cart, status: :created, location: @cart }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
+    customer = Stripe::Customer.create(
+      :email => @user.login_email,
+      :card  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'OpenSpace Customer',
+      :currency    => 'usd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    #Actionmailer here
+    redirect_to carts_path
   end
 
   # PUT /carts/1
--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -6,4 +6,9 @@ class GalleryController < ApplicationController
 	def detail
 	    @space_detail = ListSpace.find(params[:id]) #.all
 	end
+
+	def search
+		@space_gallery = ListSpace.find_by_sql ["Select * from list_spaces WHERE city like ? or state like ? or zipcode like ? or description like ?",params[:search],params[:search],params[:search],params[:search]]
+		
+	end
 end
