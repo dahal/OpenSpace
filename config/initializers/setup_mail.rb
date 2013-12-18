ae64c229b859d52243e74709e81f1d3c5feaef66
Puru Dahal <pdahal@me.com>
2013-12-18 01:49:17 -0600

    setup_mail update

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.smtp_settings ={
 	:address					=> "smtp.gmail.com",	
 	:port						=> 587,
-	:domain						=> "http://localhost:3000",
+	:domain						=> "http://open-space.herokuapp.com",
 	:user_name					=> "quickrails",
 	:password					=> "rubyonrails",
 	:authentication				=> "plain",
 	:enable_starttls_auto       => true
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "open-space.herokuapp.com"
